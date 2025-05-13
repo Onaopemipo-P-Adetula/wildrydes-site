f290a5d84310037a5b900f42d21a89c7e87e22e8
Onaopemipo-P-Adetula <dronaopemipo@gmail.com>
2025-05-13 20:43:56 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_L2CZPRNWh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2m23nt81t7rmrq1ba0bm16j2d5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-west-2_V3isyRAlj' // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1csb7jft49rskf03fdus4sjaeo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
